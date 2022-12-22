4bcea9d94ff0f7d364ce4e66a47052f083bc3c88
cactys <cactys95@yandex.ru>
2022-12-22 18:01:43 +0300

    feat: чето-там

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import './App.css';
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import './Input.css';
 
 const Input = ({ value, setText, addTodo }) => {
+  const handleInputTodo = (evt) => {
+    setText(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
   return (
     <label className="input">
-      <form className="input__form">
+      <form className="input__form" onSubmit={handleSubmit}>
         <input
           value={value}
-          onChange={(e) => setText(e.target.value)}
+          onChange={handleInputTodo}
           className="input__input-todo"
           type="text"
           placeholder="Задача"
           name="todo"
-          required
         />
         <button type="submit" onClick={addTodo} className="input__add-button">
           Добавить запись
